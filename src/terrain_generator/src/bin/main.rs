cf67a252f38a5c1b0cdbdff8b06a32a983fb390c
Morten Lohne <lohnemorten@gmail.com>
2020-10-10 22:27:48 +0200

    Allow running terrain generator from inside Rust

---
+++ src/terrain_generator/src/bin/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut terrain_gen = terrain_generator::terrain_generator::TerrainGenerator::new(None);
+
+    let points = 2u32.pow(13);
+    let radius = (500.0 / points as f64).sqrt() / 10.0;
+
+    let world = terrain_gen.world(radius, 30.0, 1.0, 1.0);
+
+    println!("{:?}", world);
+}
