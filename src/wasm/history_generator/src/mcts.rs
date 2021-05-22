07e6381c895c1f02561f5b69b163012b81132dd5
Halvard Vegum <30663324+Havegum@users.noreply.github.com>
2021-05-22 11:19:58 +0200

    changing computers

--- src/wasm/history_generator/src/mcts.rs
+++ src/wasm/history_generator/src/mcts.rs
@@ -21,10 +21,14 @@ macro_rules! log {
 struct Node {
   visited: u32,
   cumulative_value: f64,
+  board: Option<&mut Board>,
   children: HashMap<Action, Node>,
 }
 
 impl Node {
+  const ROLLOUT_DEPTH: u32 = 8;
+  const EXPLORATION_FACTOR: u32 = 2;
+
   pub fn value(&self) -> f64 {
     self.cumulative_value / self.visited as f64
   }
@@ -33,8 +37,9 @@ impl Node {
     if self.visited == 0 { f64::MAX } else { 1. }
   }
 
-  pub fn new() -> Node {
+  pub fn new(board: Option<&mut Board>) -> Node {
     Node {
+      board,
       visited: 0,
       cumulative_value: 0.,
       children: HashMap::new()
@@ -46,7 +51,7 @@ impl Node {
 
     if let Some(civ) = board.civs.get(civ) {
       for action in civ.get_actions(&board.cells).drain(..) {
-        self.children.insert(action, Node::new());
+        self.children.insert(action, Node::new(None));
       }
     }
   }
@@ -57,29 +62,44 @@ impl Node {
     let action = actions.nth(action).unwrap().clone();
     action
   }
+
+  pub fn rollout(&mut self, depth: u32) -> HashMap<usize, f64> {
+    if depth > Node::ROLLOUT_DEPTH {
+      self.value()
+    }
+
+    unimplementend!()
+
+    // simulate with random actions forever, until depth `n`, then return value.
+
+    self.rollout(depth + 1)
+  }
+
+  fn backpropagate(&mut self) {
+    unimplementend!()
+  } 
 }
 
 pub struct MCTS<'a> {
   board: &'a Board,  
 }
 
+
 impl<'a> MCTS<'a> {
   pub fn search(board: &mut Board, civ_id: usize) -> Action {
     let Board { ref mut civs, ref cells, .. } = board;
+
 
     let civ = civs.get_mut(&civ_id).unwrap();
     let actions = civ.get_actions(cells);
     let action = board.rng.next_u32() as usize % actions.len();
     let action = actions[action].clone();
     
+    let tree = Node::new(Some(board));
+    
     action
   }
 
-  // pub fn new(board: &mut Board) -> MCTS {
-  //   MCTS {
-  //     board
-  //   }
-  // }
 
   // fn select(&mut self, expansion: Expansion) {
     // SELECT some node from expansion.children
